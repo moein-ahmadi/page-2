45512d038a781b96d1237113ff394df1a29c0e3f
moein-Ahmadi <moeinahmadi57@gmail.com>
2023-12-25 20:55:20 +0330

    page2

---
+++ c-1/src/java.js
@@ -0,0 +1,22 @@
+// header
+const pagesClick = () => {
+  const x = document.querySelector(".header--li--divHidden");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+// -----------------------
+const menuClick = () => {
+  const m = document.querySelector(".header-div--nav");
+  if (m.style.display === "none") {
+    m.style.display = "block";
+  } else {
+    m.style.display = "none";
+  }
+};
+// header
+// --------------------------
+
+// -------------------------
